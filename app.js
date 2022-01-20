f100a15bfee00092f96d6e2720334d238f55ce10
petrovicivan00 <mr.petrovicivan@gmail.com>
2022-01-20 13:20:09 +0100

    token fix

--- app.js
+++ app.js
@@ -4,31 +4,17 @@ const express = require('express');
 const exphbs = require('express3-handlebars');
 const bodyparser = require('body-parser');
 var jwt = require("jsonwebtoken");
+const routes = require('./routes/routes');
 require('dotenv').config();
-const movieController = require('./controllers/movieController');
-const userController = require('./controllers/userController');
 
 var app = express();
 app.use(bodyparser.urlencoded({
     extended: true
 }));
 
-app.get('/register', (req, res) => {
-    res.redirect('user/register')
-});
-
-app.get('/login', (req, res) => {
-    res.redirect('user/login')
-});
-
-app.get('/', (req, res) => {
-    res.redirect('movie/main')
-});
-
 app.use(bodyparser.json());
 app.set('views', path.join(__dirname, '/views/'));
 app.engine('html', exphbs({ extname: 'html', defaultLayout: 'main', layoutsDir: __dirname + '/views/movie/'}));
 app.set('view engine', 'html');
-app.use('/movie', movieController);
-app.use('/user', userController);
+app.use('/', routes);
 const server = app.listen(process.env.PORT, console.log(`Server running on port ${process.env.PORT}.`))
--- controllers/movieController.js
+++ controllers/movieController.js
@@ -115,17 +115,4 @@ router.get('/delete/:id', async (req, res) => {
     });
 });
 
-function authToken(req, res, next) {
-    const authHeader = req.headers.authorization
-    const token = authHeader && authHeader.split(' ')[1]
-    if (token == null) return res.redirect(301, 'user/login');
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        console.log(err)
-        if (err) return res.redirect(301, 'user/login');
-        req.user = user
-        next()
-    })
-}
-
 module.exports = router;
